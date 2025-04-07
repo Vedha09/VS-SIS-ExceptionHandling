514967123c795eda18480be14da5b526dc2fa7c9
Vedha09 <115984518+Vedha09@users.noreply.github.com>
2025-04-07 12:21:01 +0530

    Add files via upload

---
+++ Payment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS
+{
+    internal class Payment
+    {
+        public int paymentId;
+        public Student student;
+        public decimal amount;
+        public DateTime paymentDate;
+
+        public int PaymentID { get; set; }
+        public Student Student { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+
+        public Payment(int paymentId, Student student, decimal amount, DateTime paymentDate)
+        {
+            this.paymentId = paymentId;
+            this.student = student;
+            this.amount = amount;
+            this.paymentDate = paymentDate;
+        }
+        public Student GetStudent()
+        {
+            return Student;
+        }
+        public decimal GetPaymentAmount()
+        {
+            return Amount;
+        }
+        public DateTime GetPaymentDate()
+        {
+            return PaymentDate;
+        }
+    }
+}
